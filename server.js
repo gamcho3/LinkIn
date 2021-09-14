0ac5276b412a61b8b71ef159312858660077d33b
Yong Jin <70016523+gamcho3@users.noreply.github.com>
2021-09-14 17:36:02 +0900

    remove func

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
 const express = require("express"); //express
 const connectDB = require("./config/db"); //db 연결
 const app = express();
-
+const path = require("path");
 //connect server
 connectDB();
 
 //init middleware
 app.use(express.json({ extended: false })); //데이터를 req.body 객체로 만들어주는 미들웨어
 
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
 //define Route
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/post", require("./routes/api/post"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/bulid"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
