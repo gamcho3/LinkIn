068414843634dcfce33eefa0b10656cd61319995
Yong Jin <70016523+gamcho3@users.noreply.github.com>
2021-09-15 09:16:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/post", require("./routes/api/post"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/bulid"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
