42561209d86e96be2a2cff1dd63e2818b61f91b7
Yong Jin <70016523+gamcho3@users.noreply.github.com>
2021-09-16 00:21:59 +0900

    update key

--- client/src/component/profile/ProfileAbout.js
+++ client/src/component/profile/ProfileAbout.js
@@ -1,18 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classes from "./ProfileAbout.module.css";
-import { Fragment } from "react";
+
 const ProfileAbout = ({ profile: { genres } }) => {
   return (
     <div className={classes["profile-about"]}>
       <h2>total skills</h2>
       <div className={classes.skills}>
-        {genres.map((genre, index) => (
-          <Fragment>
-            <div key={index} className={classes.skill}>
-              {genre}
-            </div>
-          </Fragment>
+        {genres.map((genre) => (
+          <div key={genre} className={classes.skill}>
+            {genre}
+          </div>
         ))}
       </div>
     </div>
